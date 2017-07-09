66205ae0110c08deae57a75a5f37a41a64f1557f
Justin Clowney <jclowney24@yahoo.com>
2017-07-08 19:37:04 -0500

    fixed config issues with LoopBack

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mustache = require('mustache-express')
 const bodyParser = require('body-parser')
 // const models = require('../common/models')
 const app = express()
-app.use(express.static(__dirname + '/public'))
+// app.use(express.static(__dirname + '/public'))
 app.engine('mustache', mustache())
 app.set('view engine', 'mustache')
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -13,9 +13,6 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }))
-app.listen(3000, function () {
-  console.log('listening')
-})
 var mainroute = require('./routes/home.js')
 var userroute = require('./routes/profile.js')
 var eventroute = require('/routes/event.js')
--- routes/profile.js
+++ routes/profile.js
@@ -6,11 +6,11 @@ const models = require('../common/models')
 router.use(bodyParser.urlencoded({ extended: false }))
 router.get('/:name', function (req, res, next) {
   var username = req.params.name
-  models.Volunteer.findOne({where: {username: username}}).then(user => {
+  models.Volunteer.findOne({where: {last_name: username}}).then(user => {
     if (user.length) {
       res.render('profile-user', {user: user})
     } else {
-      models.Organizer.findOne({where: {username: username}}).then(user => {
+      models.Organizer.findOne({where: {last_name: username}}).then(user => {
         res.render('profile-organization', {user: user})
       })
     }
